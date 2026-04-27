89fb8eab9f77ceadbeddfef8a72906bede822f7b
i am leveloth <judittelii@gmail.com>
2026-04-27 11:48:16 +0200

    cambios para activar pwa

---
+++ public/service-worker.js
@@ -0,0 +1,31 @@
+const CACHE_NAME = "readroom-cache-v1";
+
+self.addEventListener("install", (event) => {
+    self.skipWaiting();
+
+    event.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => {
+            return cache.addAll(["/"]);
+        })
+    );
+});
+
+self.addEventListener("activate", (event) => {
+    event.waitUntil(
+        caches.keys().then((keys) =>
+            Promise.all(
+                keys
+                    .filter((key) => key !== CACHE_NAME)
+                    .map((key) => caches.delete(key))
+            )
+        )
+    );
+
+    self.clients.claim();
+});
+
+self.addEventListener("fetch", (event) => {
+    event.respondWith(
+        fetch(event.request).catch(() => caches.match(event.request))
+    );
+});
